0b0a702cc2107ff9985fc049b22ecb57861ea2bf
Sukanya Roy <54613191+roysukanya16@users.noreply.github.com>
2023-06-09 21:01:57 +0530

    Create permutation-of-given-string.cpp

---
+++ GFG/permutation-of-given-string.cpp
@@ -0,0 +1,13 @@
+//q--Given a string S. The task is to print all unique permutations of the given string in lexicographically sorted order.
+
+vector<string>find_permutation(string S)
+		{
+		    // Code here there
+		    sort(S.begin(),S.end());
+		    vector<string> ans;
+		    
+		    do{
+		        ans.push_back(S);
+		    }while(next_permutation(S.begin(),S.end()));
+		    return ans;
+		}
